732ccb6181ecfa273ab512bc6f0513b862e42574
Taha sdq <mtaha@digital-dividend.com>
2026-01-06 18:46:36 +0500

    fix

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,7 @@
 
 import {User} from "../models/user.model.js"
 import { createError, UserErrorResponse } from "../utils/error.js";
-import tokenService from "./token.service.js";
+import {generateToken} from "./token.service.js";
 import type { UserLogin, UserRegister } from "../types/user.types.js";
 
 
@@ -21,7 +21,7 @@ const loginUser = async (data:UserLogin)=>{
 
     if(!userFound) {  throw  createError(UserErrorResponse.invalidCredentials.message, 401,UserErrorResponse.invalidCredentials.code);}
 
-    const userToken = await tokenService.generateToken(userFound) //generating otp token
+    const userToken = await generateToken(userFound) //generating otp token
     if(!userToken) {throw new Error }
 
     const {_id} = userFound.toObject()
