91ee24404b80de2091932c2f2681673b102eabda
Taha sdq <>
2026-01-20 17:21:18 +0500

    some changes

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,17 @@ import paymentRouter from './routes/payment.routes.js'
 import productRouter from './routes/product.routes.js'
 import cors from 'cors'
 
-dotenv.config({ path: process.env["NODE_ENV"] =="local" ? '.env.local' : '.env.development' }) //set path from root of folder
+dotenv.config({ path: process.env?.["NODE_ENV"] =="local" ? '.env.local' : '.env.development' }) //set path from root of folder
 const app = express()
 
 //connect my nodejs app with mongodb
-mongoose.connect(process.env["MONGO_DB_URL"] ?? "") 
+mongoose.connect(process.env?.["MONGO_DB_URL"] ?? "") 
 .then(()=>console.log("connected with mongodb"))
 .catch((err)=>console.log("error connecting to mongodb", err))
 
 
 
-app.use(cors(  {origin: "http://localhost:3000",credentials: true})) // if you will change it and if it cause it error it will show internet connection error to user
+app.use(cors(  {origin: process.env?.["FRONTEND_URL"],credentials: true})) // if you will change it and if it cause it error it will show internet connection error to user
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.set('trust proxy', 1);
